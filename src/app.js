ec6199a699039e8d25d765f698dcafdfcf54c14a
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-03-14 20:04:30 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,44 @@
 import express from "express";
-import ProductManager from "./desafioJsBackend.js";
+import productRouter from "./routes/products.router.js";
+import cartRouter from "./routes/cart.router.js";
+import {engine} from "express-handlebars";
+import __dirname from "./utils.js";
+import {Server} from "socket.io";
 
 const app = express();
-let manager = new ProductManager("./prod.json");
 
-app.get("/products", async (req, res) => {
-    res.send(manager.getProduct());
+app.use(express.static(__dirname + "/../public"))
+
+app.use("/api/products", productRouter);
+app.use("/api/cart", cartRouter);
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views',__dirname+'/views');
+
+
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-app.get("/products/:id", async (req, res) => {
-    let numeroProduct = parseInt(req.params.id);
-    res.send(manager.getProductById(numeroProduct));
+/*
+app.listen(8080, ()=>{
+    console.log("Server listening on port 8080");
+});*/
+
+const httpServer = app.listen(8080, ()=>{
+    console.log("Server listening on port 8080");
 });
 
-app.get("/products",async  (req, res) => {
-    const producto= await manager.getProducts()
-   let { limit } = req.query;
-    res.send(producto.filter((x) => x.id <= limit));
-});
-  
-app.listen(8080, () => {
-    console.log(`Server listening to port 8080`)
+const socketServer = new Server(httpServer);
+
+socketServer.on('connection', (socket)=>{
+    console.log("Connection established");
+
+
+    socket.on("message", (data)=>{
+        console.log(data);
+
+        socket.emit("message", "mensaje desde el server");
+    });
 });
