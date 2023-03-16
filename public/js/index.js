bc4a8cb470390b74ebf9fd8d77c16eb5eb51eabe
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-03-16 07:43:12 -0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,14 @@
 const socket = io();
 
-socket.emit('message', "hola mundo!");
-
-socket.on("message", (data) => {
-    alert(data);
+const datosProd = document.getElementById("listaProd");
+socket.on("listaProd", async(d)=>{
+    let prodLista = "";
+    await d.forEach((e)=>{
+        prodLista+= `
+        <ul>
+            <li>Producto:${e.title}, Codigo:${e.price}, Precio:${e.price}, Stock:${e.stock}, Categoria:${e.description}</li>
+        </ul>    
+        `
+    });
+    datosProd.innerHTML = prodLista;
 });
