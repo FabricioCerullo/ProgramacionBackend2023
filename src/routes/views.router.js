b41dfee064886c768d5917f039e1442bdc4f9af4
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-03-16 21:05:16 -0300

    Update views.router.js

---
+++ src/routes/views.router.js
@@ -0,0 +1,19 @@
+import { json, Router } from "express";
+import ProductManager from "../managers/desafioJsBackend.js";
+
+
+const viewsRouter = Router();
+let manager = new ProductManager();
+viewsRouter.use(json());
+
+viewsRouter.get("/", async (req,res)=>{
+    const prod = await manager.getProducts()
+    res.render("home", {prod})
+})
+
+viewsRouter.get("/real-time-products", async (req,res)=>{
+    const prod = await manager.getProducts()
+    res.render("real-time-products", {prod})             
+})
+
+export default viewsRouter;
