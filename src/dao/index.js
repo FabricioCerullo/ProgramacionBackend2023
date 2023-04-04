e5918fdcbe2f752ac97b4dc1f563ced99ae97bc6
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-04-03 22:56:48 -0300

    Update index.js

--- src/dao/index.js
+++ src/dao/index.js
@@ -1,18 +1,23 @@
 import FileProductManager from "./managers/desafioJsBackend.js";
+import FileCartManager from "./managers/cart.js"
 import DBProductManager from "./db_managers/desafioJsBackend.js";
+import DBCartManager from "./db_managers/cart.js"
 
 const config = {
-    persistenceType: "db"
+    persistenceType: "db",
 };
 
 let ProductManager;
+let CartManager;
 
 if (config.persistenceType ==="db") {
     ProductManager = DBProductManager;
+    CartManager = DBCartManager;
 }else if (config.persistenceType ==="file"){
     ProductManager = FileProductManager;
+    CartManager = FileCartManager;
 }else{
-    throw new Error("Error")
+    throw new Error("Error");
 }
 
-export {ProductManager};
+export { ProductManager, CartManager };
