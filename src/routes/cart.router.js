c06b146c067998e26dcee12cf0c2204375ebbe95
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-04-04 20:12:57 -0300

    Update cart.router.js

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -4,26 +4,39 @@ import {ProductManager} from "../dao/index.js";
 
 
 const cartRouter = Router();
-let manager = new CartManager();
+const manager = new CartManager();
 cartRouter.use(json());
 
 
+//devuelve todos los carritos 
 
 cartRouter.get('/', async (req, res)=>{
    const carro = await manager.getCart();
     res.send({status:"sucess", payload: carro});
 })
 
+//agrego 1 carro con sus respectivos prod.
+
 cartRouter.post('/', async (req, res)=>{
-    await manager.addNewCart();
-    res.send({status:"sucess", payload: "carro añadido"});
+    const {cid, products} = req.body;
+    const newCarr = await manager.addNewCart(cid, products);
+    res.send({status:"sucess", payload: newCarr});
 })
 
+//busca 1 carro por la cid
+
 cartRouter.get('/:cid', async (req, res) => {
-    const {cid} = req.params;
-    let carro = await manager.findCartToID(cid);
-    res.send({status:"sucess", payload: carro});
+    try {
+        const {cid} = req.params;
+        const carro = await manager.findCartToID(parseInt(cid));
+        res.send({status:"sucess", payload: carro});
+    } catch (error) {
+        res.status(404).send({status: "error", error: "Ha ocurrido un error!"});
+    }
+
 })
+
+//añadir 1 producto mas en el carro buscado por CID
 
 cartRouter.post('/:cid/product/:pid', async(req, res)=>{
     const {cid, pid} = req.params;
@@ -42,5 +55,6 @@ cartRouter.delete('/:cid/product/:pid', async (req,res)=>{
     let prodDelete = await manager.deleteProductById(prodid);
     res.send({status:"sucess", payload: prodDelete});
 })
+
 
 export default cartRouter;
