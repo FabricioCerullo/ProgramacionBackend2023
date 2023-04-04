278feba729b68a2fbd2fed6fea9d0707dd94c434
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-04-03 22:56:39 -0300

    Update cart.js

--- src/dao/db_managers/cart.js
+++ src/dao/db_managers/cart.js
@@ -1,22 +1,30 @@
-export default class DBCartManager{
-    #acc=0;
+import cartModel from "../models/cart.model.js";
+export default class CartManager{
     constructor(){
-
+        console.log("Working with products using Data Base");
     }
 
     async getCart(){
-
+        const carroCompra = await cartModel.find().lean();
+        return carroCompra;
     }
 
     async addNewCart(){
+        const carrAdd  = {
+            cid :1,
+            products: [],
+        }
 
+        const result = await cartModel.create(carrAdd);
+        return result;
     }
 
-    async findCartToID(cid) {
-
+    async deletProdInCart(cid){
+        const cart = await cartModel.find({_id:cid});
+        let prodInCart = cart[0].products;
+        console.log(prodInCart);
+        return prodInCart;
     }
 
-    async addProductToCart(cid, product) {
-    
-}
+
 }
