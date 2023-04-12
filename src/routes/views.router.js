72a382c3ccbeed496f4acf3ed91d24e020f0df77
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-04-11 21:07:31 -0300

    Update views.router.js

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -3,13 +3,15 @@ import {ProductManager} from "../dao/index.js";
 
 
 const viewsRouter = Router();
-let manager = new ProductManager();
+const manager = new ProductManager();
 viewsRouter.use(json());
+
 
 viewsRouter.get("/", async (req,res)=>{
     const prod = await manager.getProducts()
     res.render("home", {prod})
 })
+
 
 viewsRouter.get("/real-time-products", async (req,res)=>{
     const prod = await manager.getProducts()
