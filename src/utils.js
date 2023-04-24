73fb8e023daea9b19208af7c4c4a17999e21a6f1
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-04-24 00:24:46 -0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,17 @@
 import path from "path";
 import { fileURLToPath } from "url";
-
+import bcrypt from "bcrypt";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default __dirname;
+
+
+export const createHash = (password)=>{
+    return bcrypt.hashSync(password, bcrypt.genSaltSync());
+}
+
+export const isValidPassword = (user, logPassword)=>{   
+   return bcrypt.compareSync(logPassword, user.password);
+}
