1e0d78a43ad6307933f9089421b5e6c979bc067f
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-05-11 21:48:27 -0300

    Update cart.router.js

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -3,96 +3,21 @@ import {CartManager} from "../dao/index.js";
 import {ProductManager} from "../dao/index.js";
 import mongoose from "mongoose";
 
-
+import { getCartController, deleteProdInCartController,addProdInCartController,updateCartController,deleteCartController} from "../controller/index.controller.js";
 const cartRouter = Router();
-const manager = new CartManager();
-const managerProd = new ProductManager();
+//const manager = new CartManager();
+//const managerProd = new ProductManager();
 cartRouter.use(json());
 
-
 //devuelve todos los carritos 
-
-cartRouter.get('/', async (req, res)=>{
-   const carro = await manager.getCart();
-    res.send({status:"sucess", payload: carro});
-})
-
+cartRouter.get('/',getCartController )
 //elimina del carro el prod. seleccionado
-
-cartRouter.delete("/:cid/products/:pid", async (req, res)=>{
-    const {cid,pid} = req.params;
-    const prodDel = await manager.deleteProd(cid, pid);
-    res.send({status:"success", payload: prodDel});
-
-})
-
+cartRouter.delete("/:cid/products/:pid",deleteProdInCartController)
 //agrega prod. al carro
-
-cartRouter.post('/:cid/:pid', async (req, res)=>{
-    const {cid, pid} = req.params;
-    const result = await manager.addProductToCart(cid, pid);
-    res.send({status:"success", payload: result});
-})
-
+cartRouter.post('/:cid/:pid',addProdInCartController)
 //actualiza la cart.
-
-cartRouter.put('/:cid', async (req, res)=>{
-    const {cid,pid} = req.params;
-    const result = await manager.updateCart(cid,pid);
-    res.send({status:"success", payload: result});
-})
-
+cartRouter.put('/:cid',updateCartController)
 //elimina los prod. de la cart.
-
-cartRouter.delete("/:cid", async (req, res) => {
-    const {cid} = req.params;
-    const result = await manager.deleteCart(cid);
-    res.send({status:"success", payload: result});
-})
-
-
-/*
-
-//agrego 1 carro con sus respectivos prod.
-
-cartRouter.post('/', async (req, res)=>{
-    const {cid, products} = req.body;
-    const newCarr = await manager.addNewCart(cid, products);
-    res.send({status:"sucess", payload: newCarr});
-})
-
-//busca 1 carro por la cid
-
-cartRouter.get('/:cid', async (req, res) => {
-    try {
-        const {cid} = req.params;
-        const carro = await manager.findCartToID(parseInt(cid));
-        res.send({status:"sucess", payload: carro});
-    } catch (error) {
-        res.status(404).send({status: "error", error: "Ha ocurrido un error!"});
-    }
-
-})
-
-//añadir 
-
-cartRouter.post('/:cid/product/:pid', async(req, res)=>{
-    const {cid, pid} = req.params;
-    const cartid = parseInt(cid);
-    const prodid = parseInt(pid);
-    const prod = await managerProd.getProductById(prodid);
-    const newCart = await manager.addProductToCart(cartid, prod);
-    res.send({status:"sucess", payload: newCart});
-})
-
-cartRouter.delete('/:cid/product/:pid', async (req,res)=>{
-    const {cid, pid} = req.params;
-    const cartid = parseInt(cid);
-    const prodid = parseInt(pid);
-
-    let prodDelete = await manager.deleteProductById(prodid);
-    res.send({status:"sucess", payload: prodDelete});
-})
-*/
+cartRouter.delete("/:cid",deleteCartController)
 
 export default cartRouter;
