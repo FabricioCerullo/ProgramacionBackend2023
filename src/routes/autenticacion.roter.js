a759d1385a54484c6fa965fc8cab52be47e25754
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-05-11 21:48:22 -0300

    Update autenticacion.roter.js

--- src/routes/autenticacion.roter.js
+++ src/routes/autenticacion.roter.js
@@ -1,140 +1,16 @@
 import { Router} from "express";
 import passport from "passport";
-
-import { userModel } from "../dao/models/user.model.js";
-import { createHash, isValidPassword } from "../utils.js";
-import jwt from "jsonwebtoken"
-import { options } from "../config/options.js";
+import { registroRedirectController,failedRedirectController,loginRedirectController,gitHubRedirectController,currentRedirectController,forgotRedirectController,logoutRedirectController,authMiddleware } from "../controller/index.controller.js";
 
 const router = Router();
 
-//middleware de autenticacion
-const authMiddleware = async (req, res, next) => {
-    if (req.isAuthenticated()) {
-        const userId = req.session.passport.user;
-        const user =await userModel.findById(userId)
-          if (!user) return res.status(401).json({ error: 'No se ha iniciado sesión.' });
-          req.user = user;
-          next();
-}}
-//rutas de autenticacion
-
-//registro con passport
-
-router.post("/registro", passport.authenticate("registroStrategy", {
-    failureRedirect: "/api/sessions/failed",
-}), async (req, res) => {
-    res.redirect("/login");
-}
-)
-
-
-router.post("/login", passport.authenticate("loginStrategy", {
-    failureRedirect: "/api/sessions/failed",
-}), async (req, res) => {
-    res.redirect("/perfil");
-})
-
-router.get("/github", passport.authenticate("github", {scope: ["user:email"] }),
-async (req,res)=>{
-    //
-}
-);
-
-router.get("/github-callback", passport.authenticate("github",{failureRedirect:"/api/sessions/failed"}),
-async (req,res)=>{
-    req.session.user = req.user;
-    //console.log(req.session.user = req.user);
-    res.redirect("/");
-}
-)
-
-router.get("/current",authMiddleware,(req, res) => {
-    res.status(200).json({ user: req.user });
-}
-)
-
-//route para fallas 
-router.get("/failed", async(req, res) => {
-    console.log("falla!... algo salio mal");
-    res.status(500).send("error");
-})
-
-
-router.post("/forgot", async(req, res) => {
-    try {
-        const {email,password} = req.body;
-        const user = await userModel.findOne({email:email});
-
-        if (user) {
-            user.password = createHash(password);
-            const userUpdate = await userModel.findOneAndUpdate({email:user.email}, user);
-            res.status(200).send("Contraseña actualizada!")
-
-        }else{
-           res.status(401).send("no existe el correo");
-        }
-    } catch (error) {
-        res.status(401).send("error");
-    }
-
-})
-
-
-router.get("/logout", (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.status(500).send("Internal server error");
-          }
-          res.redirect("/login");
-    })
-})
-
-//registro y login ---- 
- /*
-
-router.post('/registro', async (req, res) => {
-    try {
-        const {first_name, last_name, email, age, password} = req.body;
-        const newUser = {
-            first_name,
-            last_name,
-            email, 
-            age,
-            password:createHash(password)
-        };
-        const userCreated = await userModel.create(newUser);
-        res.send( `Hola!, Bienvenido: ${userCreated.first_name}`)
-        }catch (error) {
-        res.status(401).send("error");
-    }
-});
-
-
-router.post('/login', async (req, res) => {
-    try {
-        const {email,password} = req.body;
-        const user = await userModel.findOne({email:email});
-
-        if (user) {
-
-            if (isValidPassword(user, password)) {
-                req.session.user = user.email;
-                const domain = email.split('@')[1];
-                if (domain==="admin.com") {
-                    res.status(200).send("el usuario es valido, es admin.");
-                    res.redirect("/perfil");
-                }else {
-                    res.send("Login Exitoso!")
-            }
-        }else{
-           res.status(401).send("no existe el correo");
-        }
-    }
-    } catch (error) {
-        res.status(401).send("error");
-    }
-})
-*/
+router.post("/registro", passport.authenticate("registroStrategy", {failureRedirect: "/api/sessions/failed",}),registroRedirectController)
+router.post("/login", passport.authenticate("loginStrategy",{failureRedirect: "/api/sessions/failed",}),loginRedirectController)
+router.get("/github", passport.authenticate("github", {scope: ["user:email"] }),async (req,res)=>{});
+router.get("/github-callback", passport.authenticate("github",{failureRedirect:"/api/sessions/failed"}),gitHubRedirectController)
+router.get("/current",authMiddleware,currentRedirectController)
+router.get("/failed", failedRedirectController)
+router.post("/forgot",forgotRedirectController)
+router.get("/logout",logoutRedirectController)
 
 export {router as AutenRouter};
