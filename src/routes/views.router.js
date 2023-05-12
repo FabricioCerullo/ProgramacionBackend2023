82607333ec6103db60b8d5b617757db2da643397
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-05-11 21:48:32 -0300

    Update views.router.js

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,46 +1,27 @@
 import { json, Router } from "express";
 import {ProductManager} from "../dao/index.js";
 
+import { homeRenderProductsController,realTimeProductsRenderController,productIDRenderController,loginController,perfilController,registroController,forgotController} from "../controller/index.controller.js";
 
 const viewsRouter = Router();
 const manager = new ProductManager();
 viewsRouter.use(json());
 
-//productos
+                //VISTA DE LOS PRODUCTOS
 
-viewsRouter.get("/", async (req,res)=>{
-    const prod = await manager.getProducts()
-    res.render("home", {prod})
-})
+//vista "home" de los productos
+viewsRouter.get("/",homeRenderProductsController)
+//vista realTimeProducts
+viewsRouter.get("/real-time-products",realTimeProductsRenderController)
+//vista de los productos por ID
+viewsRouter.get("/product/:pid",productIDRenderController)
 
-viewsRouter.get("/real-time-products", async (req,res)=>{
-    const prod = await manager.getProducts()
-    res.render("real-time-products", {prod})             
-})
+            //VISTA DE LOS USUARIOS
 
-viewsRouter.get("/product/:pid", async (req,res)=>{
-    const {pid} = req.query;
-    const prod = await manager.getProductById(pid);
-    res.render("home", {prod});
-})
-
-//usuarios
-
-viewsRouter.get('/login', (req, res) => {
-    res.render("login");
-})
-
-viewsRouter.get('/perfil', (req, res) => {
-    res.render("perfil");
-})
-
-viewsRouter.get('/registro', (req, res) => {
-    res.render("registro");
-})
-
-viewsRouter.get('/forgot', (req, res) => {
-    res.render("forgot");
-})
+viewsRouter.get('/login',loginController)
+viewsRouter.get('/perfil', perfilController)
+viewsRouter.get('/registro',registroController)
+viewsRouter.get('/forgot',forgotController )
 
 
 export default viewsRouter;
