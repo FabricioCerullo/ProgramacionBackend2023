8578b943a5c687158859abcf1c6ff380fa477fa6
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-05-27 07:16:12 -0300

    Update user.model.js

--- src/dao/models/user.model.js
+++ src/dao/models/user.model.js
@@ -31,6 +31,10 @@ const userSchema = new mongoose.Schema({
     cart:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"carts"
+    },
+    nombreCompleto:{
+        type:String,
+        default:""
     }
 })
 
