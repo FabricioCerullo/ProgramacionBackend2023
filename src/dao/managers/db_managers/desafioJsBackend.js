2385121c2e62b69d95d5ba1b0dd027f1a5912f5a
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-05-27 07:15:55 -0300

    Create desafioJsBackend.js

---
+++ src/dao/managers/db_managers/desafioJsBackend.js
@@ -0,0 +1,34 @@
+//paso 1 - luego pasar a service
+import prodModel from "../../models/prod.model.js";
+export class ProductManager{
+
+    constructor(){
+        console.log("Working with products using Data Base");
+    }
+
+    async addProduct(prod){
+        const addProd = await prodModel.create(prod);
+        return addProd;
+    }
+
+    async getProducts(){
+     const prod = await prodModel.find().lean();
+        return prod;
+
+    }
+    async deleteProduct(id){
+        const productDelete= await prodModel.deleteOne({ _id: id })
+        return productDelete;
+    }
+    
+    async getProductById(pid){
+        try {
+            const findProd = await prodModel.findById(pid)
+            return findProd;            
+        } catch (error) {
+            return error;
+        }
+
+    }
+}
+
