e7d5f9c441a6ee8a9e7da0cacf6dbb51ceaace09
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-05-30 22:43:16 -0300

    Update products.router.js

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,7 +2,7 @@ import { Router, json} from "express";
 import __dirname from "../utils.js";
 import prodModel from "../dao/models/prod.model.js";
 
-import {checkRole, getProductsController, getProductIDController,addProductController, deleteProductController, prodDTO} from "../controller/index.controller.js";
+import {checkRole, getProductsController, mockingProducts,addProductController, deleteProductController, prodDTO} from "../controller/index.controller.js";
 const productRouter = Router();
 
 //const manager = new ProductManager();
@@ -10,14 +10,17 @@ productRouter.use(json());
 
 //devuelve todos los productos
 productRouter.get('/',getProductsController)
+//ruta de mocking, genera 100 products
+productRouter.get("/mockingproducts’", mockingProducts)
 //devuelve el prod. con la id indicada
 //productRouter.get("/:id",getProductIDController)
 //devuelve el DTO de prod. se inhabilito mientras tanto la anterior
 productRouter.get("/:id",checkRole(["admin"]), prodDTO)
 //se agrega un nuevo prod. 
-productRouter.post("/", checkRole(["admin"]),addProductController)
+productRouter.post("/",addProductController)
 //se elimina el prod. especificado con la id.
 productRouter.delete("/:pid",checkRole(["admin"]),deleteProductController)
+
 
 
 export default productRouter;
