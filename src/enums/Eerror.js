a3654afa69ff4b20536bb443a8e9bf19b8292901
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-05-30 22:49:06 -0300

    Update Eerror.js

--- src/enums/Eerror.js
+++ src/enums/Eerror.js
@@ -1,7 +1,8 @@
 export const Eerror = {
-    INVALID_TYPES_ERROR:1,
-    DATABASE_ERROR:2,
-    AUTH_ERROR:3,
-    INVALID_FIELD_ERROR:4,
+    INVALID_TYPES_ERROR:1, // tipos de datos invalidos
+    DATABASE_ERROR:2, // error de base de datos
+    AUTH_ERROR:3, // error de autenticacion
+    INVALID_FIELD_ERROR:4, //error de campos invalidos
+    
 }
 
