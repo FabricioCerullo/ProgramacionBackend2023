ccdec55234f06164a5438e416d49bc80a72fc80c
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-06-24 20:30:29 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,12 +13,15 @@ import cartRouter from "./routes/cart.router.js";
 import __dirname from "./utils.js";
 import { initializePassport } from "./config/passport.config.js";
 import { options } from "./config/options.js";
-import cookieParser from "cookie-Parser";
+import cookieparser from "cookie-parser";
 import { connectDB } from "./config/db_Connetions.js";
 import { errorIndex } from "./middlewares/error/index.error.js";
 import { addLogger , logger} from "./utils/logger.js";
 import { Plogger } from "./controller/index.controller.js";
 import userRouter from "./routes/users.routes.js";
+import { swaggerSpecs } from "./config/options.js";
+import swaggerUI from "swagger-ui-express"
+
 
 const app = express();
 
@@ -30,7 +33,7 @@ const httpServer = app.listen(8080, ()=>{
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cookieParser());
+app.use(cookieparser());
 
 //configuracion de la sesion
 app.use(session({
@@ -60,7 +63,8 @@ app.use("/api/products", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/", viewsRouter);
 app.use("/api/sessions", AutenRouter);
-app.use("/api/users", userRouter)
+app.use("/api/users", userRouter);
+app.use("/api/docs", swaggerUI.serve,swaggerUI.setup(swaggerSpecs));
 
 //logger
 
