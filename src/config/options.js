52e9cae16b528be5f98b3bf26db256f038245405
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-06-28 22:13:11 -0300

    Update options.js

--- src/config/options.js
+++ src/config/options.js
@@ -62,9 +62,9 @@ const swaggerOptions ={
             description:"api rest para gestionar productos",
             version:"1.0.0"
         },
-       // servers:[{url:`http://localhost:8080`}]
+        servers:[{url:`http://localhost:8080`}]
     },
-    apis:["../docs/products/products.yaml"]
+    apis:yamlFiles
 };
 console.log(swaggerOptions.apis)
 export const swaggerSpecs = swaggerJSDoc(swaggerOptions);
