17eef04e5eb4d288d949998660f76a39410af128
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-07-19 23:36:42 -0300

    Update autenticacion.roter.js

--- src/routes/autenticacion.roter.js
+++ src/routes/autenticacion.roter.js
@@ -1,10 +1,11 @@
 import { Router} from "express";
 import passport from "passport";
 import {forgotPassword,registroRedirectController,failedRedirectController,loginRedirectController,gitHubRedirectController,currentRedirectController,forgotRedirectController,logoutRedirectController,authMiddleware,prodDTO } from "../controller/index.controller.js";
+import { uploaderProfile } from "../utils.js";
 
 const router = Router();
 
-router.post("/registro", passport.authenticate("registroStrategy", {failureRedirect: "/api/sessions/failed",}),registroRedirectController)
+router.post("/registro", uploaderProfile.single("avatar"),passport.authenticate("registroStrategy", {failureRedirect: "/api/sessions/failed",}),registroRedirectController)
 router.post("/login", passport.authenticate("loginStrategy",{failureRedirect: "/api/sessions/failed",}),loginRedirectController)
 router.get("/github", passport.authenticate("github", {scope: ["user:email"] }),async (req,res)=>{});
 router.get("/github-callback", passport.authenticate("github",{failureRedirect:"/api/sessions/failed"}),gitHubRedirectController)
