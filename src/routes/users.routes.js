8c9f2f35327bad4b411e81bd7ee3851b31a92920
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-07-19 23:36:44 -0300

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { changeRoleUser } from "../controller/index.controller.js";
+import { changeRoleUser,userDocuments, userChangeWDocument } from "../controller/index.controller.js";
+import { uploaderDocuments } from "../utils.js";
 
 const userRouter =  Router();
 
 userRouter.put("/premium/:uid", changeRoleUser);
+//userRouter.post("/:uid/documents", uploaderDocuments.array("document"), userDocuments);
+userRouter.post("/:uid/documents", uploaderDocuments.fields([{name:"identificacion", maxCount:1}, {name:"domicilio", maxCount:1},{name:"estadoDeCuenta", maxCount:1}]), userChangeWDocument);
 
 export default userRouter;
