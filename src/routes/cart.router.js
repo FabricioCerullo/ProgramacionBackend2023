e7b4bf44d2d8444dd8b5d5372615cc691fcbeacf
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-08-06 19:42:07 -0300

    Update cart.router.js

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,7 +1,7 @@
 import { Router, json } from "express";
 import mongoose from "mongoose";
 
-import {authMiddleware,createCart,checkRole, getCartController, deleteProdInCartController,addProdInCartController,deleteCartController, purchaseCart} from "../controller/index.controller.js";
+import {initiatePurchase,authMiddleware,createCart,checkRole, getCartController, deleteProdInCartController,addProdInCartController,deleteCartController, purchaseCart} from "../controller/index.controller.js";
 const cartRouter = Router();
 //const manager = new CartManager();
 //const managerProd = new ProductManager();
@@ -15,10 +15,11 @@ cartRouter.delete("/:cid/products/:pid",deleteProdInCartController)
 //crea la cart.
 cartRouter.post("/", createCart)
 //agrega prod. y actualiza la cart.
-cartRouter.put('/:cid/:pid',addProdInCartController)
+cartRouter.post('/add/:cid/:pid',addProdInCartController)
 //elimina los prod. de la cart.
 cartRouter.delete("/:cid",deleteCartController)
 //ruta purchase
-cartRouter.post('/:cid/purchase',purchaseCart)
+cartRouter.post('/purchase/:cid',purchaseCart)
+cartRouter.get('/initial-purchase',initiatePurchase)
 
 export default cartRouter;
