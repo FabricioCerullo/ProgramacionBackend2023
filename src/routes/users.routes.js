b29b6b6fd7b4198be840ffc09c2788dd8567ef6d
Fabricio Cerullo <fabriciocerullo73@gmail.com>
2023-08-06 19:42:15 -0300

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { changeRoleUser,userDocuments, userChangeWDocument } from "../controller/index.controller.js";
+import {deleteUser, changeRoleUser,userDocuments, userChangeWDocument,userAllDB,deleteUserTime } from "../controller/index.controller.js";
 import { uploaderDocuments } from "../utils.js";
 
 const userRouter =  Router();
 
-userRouter.put("/premium/:uid", changeRoleUser);
-//userRouter.post("/:uid/documents", uploaderDocuments.array("document"), userDocuments);
+//userRouter.put("/premium/:uid", changeRoleUser);
+userRouter.post("/:uid/documents", uploaderDocuments.array("document"), userDocuments);
 userRouter.post("/:uid/documents", uploaderDocuments.fields([{name:"identificacion", maxCount:1}, {name:"domicilio", maxCount:1},{name:"estadoDeCuenta", maxCount:1}]), userChangeWDocument);
-
+userRouter.get("/",userAllDB);
+userRouter.delete("/",deleteUserTime);
+userRouter.post("/delete",deleteUser)
+userRouter.post("/premium", changeRoleUser);
 export default userRouter;
